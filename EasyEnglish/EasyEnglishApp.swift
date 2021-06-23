5263f222aa94962e776630f03797517e2e00f1c6
akurbo <artem-0072009@yandex.ru>
2021-06-23 19:07:26 +0300

    Initial Commit

---
+++ EasyEnglish/EasyEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyEnglishApp.swift
+//  EasyEnglish
+//
+//  Created by Kurbatov Artem on 23.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct EasyEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
