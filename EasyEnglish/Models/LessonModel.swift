9c47e46c98ef17fcd357c3d8a2c719eaa06b4f5c
akurbo <artem-0072009@yandex.ru>
2021-06-23 19:40:03 +0300

    -

--- EasyEnglish/Models/LessonModel.swift
+++ EasyEnglish/Models/LessonModel.swift
@@ -12,14 +12,12 @@ struct Module: Decodable, Identifiable {
     var id: Int
     var category: String
     var content: Content
-    var test: Test
 }
 
 struct Content: Decodable, Identifiable {
     
     var id: Int
     var image: String
-    var time: String
     var description: String
     var lessons: [Lesson]
     
@@ -29,25 +27,6 @@ struct Lesson: Decodable, Identifiable {
     
     var id: Int
     var title: String
-    var video: String
-    var duration: String
     var explanation: String
     
-}
-
-struct Test: Decodable, Identifiable {
-    
-    var id: Int
-    var image: String
-    var time: String
-    var description: String
-    var questions: [Question]
-}
-
-struct Question: Decodable, Identifiable {
-    
-    var id: Int
-    var content: String
-    var correctIndex: Int
-    var answers: [String]
 }
--- EasyEnglish/ViewModels/ContentModel.swift
+++ EasyEnglish/ViewModels/ContentModel.swift
@@ -9,4 +9,42 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    @Published var tests = [Test]()
+    
+    init() {
+        getLessonData()
+        getTestData()
+    }
+    
+    func getLessonData() {
+        
+        let url = Bundle.main.url(forResource: "lessonData", withExtension: "json")
+        do{
+            let data = try Data(contentsOf: url!)
+            let decoder = JSONDecoder()
+            let modules = try decoder.decode([Module].self, from: data)
+            self.modules = modules
+        }
+        catch{
+            print(error)
+        }
+        
+    }
+    
+    func getTestData() {
+        
+        let url = Bundle.main.url(forResource: "testData", withExtension: "json")
+        do{
+            let data = try Data(contentsOf: url!)
+            let decoder = JSONDecoder()
+            let modules = try decoder.decode([Module].self, from: data)
+            self.modules = modules
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    
 }
