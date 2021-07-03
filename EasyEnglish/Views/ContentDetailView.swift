c6e63a18f520496bbfc3bbef891e91f6c2b47c56
akurbo <artem-0072009@yandex.ru>
2021-07-03 10:25:25 +0300

    add lessons data

---
+++ EasyEnglish/Views/ContentDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentDetailView.swift
+//  EasyEnglish
+//
+//  Created by Kurbatov Artem on 03.07.2021.
+//
+
+import SwiftUI
+
+struct ContentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentDetailView()
+    }
+}
---
+++ EasyEnglish/Views/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  EasyEnglish
+//
+//  Created by Kurbatov Artem on 23.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var body: some View {
+        ScrollView{
+            
+            LazyVStack {
+                
+                if model.currentModule != nil {
+                    ForEach(0..<model.currentModule!.content.lessons.count) { index in
+                        
+                        let lesson = model.currentModule!.content.lessons[index]
+                    
+                        ZStack(alignment: .leading){
+                        
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .frame(height: 66)
+                        
+                        HStack{
+                            
+                            Text(String(index+1))
+                                .bold()
+                            
+                            VStack(alignment: .leading){
+                                Text(lesson.title)
+                                    .bold()
+                                
+                            }
+                        }
+                        .padding(.leading, 10)
+                        }
+                            .padding(.bottom, 10)
+                    
+                }
+                }
+            }
+            .padding()
+            .navigationTitle(model.currentModule?.category ?? "")
+        }
+    }
+}
