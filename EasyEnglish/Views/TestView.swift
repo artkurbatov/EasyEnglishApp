de702f04dc22d232e32dceb85671abb7ed306aa1
akurbo <artem-0072009@yandex.ru>
2021-07-04 22:55:47 +0300

    testView

--- EasyEnglish/Views/TestView.swift
+++ EasyEnglish/Views/TestView.swift
@@ -11,19 +11,105 @@ struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
     
+    @State var selectedAnswerIndex: Int?
+    @State var result = 0
+    @State var submitted = false
+    
     var body: some View {
         
         if model.currentQuestion != nil {
             
-            VStack{
+            VStack(alignment: .leading){
                 Text("Вопрос \(model.currentQuestionIndex + 1) из \(model.currentModule?.test.questions.count ?? 0)\n")
+                    .padding(.leading, 20)
+                    .padding(.top, 10)
                 
                 Text(model.currentQuestion!.content)
+                    .padding(.horizontal, 20)
                 
-                Spacer()
+                ContentDescriptionView()
                 
-                //ContentDescriptionView()
+                ScrollView{
+                    VStack{
+                        ForEach(0..<model.currentQuestion!.answers.count, id: \.self) { index in
+                            
+                            Button(action: {
+                                selectedAnswerIndex = index
+                            }, label: {
+                                ZStack{
+                                    if submitted == false{
+                                Rectangle()
+                                    .foregroundColor(index == selectedAnswerIndex ? .gray : .white)
+                                    .cornerRadius(10)
+                                    .shadow(radius: 5)
+                                    .frame(height: 48)
+                                    }
+                                    else{
+                                        if index == selectedAnswerIndex && index == model.currentQuestion!.correctIndex{
+                                            Rectangle()
+                                                .foregroundColor(.green)
+                                                .cornerRadius(10)
+                                                .shadow(radius: 5)
+                                                .frame(height: 48)
+                                        }
+                                        else if index == selectedAnswerIndex && index != model.currentQuestion!.correctIndex{
+                                            Rectangle()
+                                                .foregroundColor(.red)
+                                                .cornerRadius(10)
+                                                .shadow(radius: 5)
+                                                .frame(height: 48)
+                                        }
+                                        else if index == model.currentQuestion!.correctIndex{
+                                            Rectangle()
+                                                .foregroundColor(.green)
+                                                .cornerRadius(10)
+                                                .shadow(radius: 5)
+                                                .frame(height: 48)
+                                        }
+                                        else{
+                                            Rectangle()
+                                                .foregroundColor(.white)
+                                                .cornerRadius(10)
+                                                .shadow(radius: 5)
+                                                .frame(height: 48)
+                                        }
+                                    }
+
+                                    Text(model.currentQuestion!.answers[index])
+                            }
+                            })
+                            .disabled(submitted)
+                            
+                           
+                        }
+                    }
+                    .accentColor(.black)
+                    .padding()
+                }
                 
+                Button(action: {
+                    submitted = true
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex{
+                        result += 1
+                    }
+                    
+                }, label: {
+                    ZStack{
+                    Rectangle()
+                        .foregroundColor(.green)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .frame(height: 48)
+                        
+                        Text("Ответить")
+                            .bold()
+                            .foregroundColor(.white)
+                            
+                }
+                    .padding()
+                })
+                .disabled(selectedAnswerIndex == nil)
+                //Spacer()
             }
             .navigationBarTitle("Тест \(model.currentModule?.category ?? "")")
         }
